e0d394060a6ae0f638dfc821db6feeeedb61a9c4
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-07-29 13:56:35 +0300

    Link `file` model to `user` model

--- internal/db/file.go
+++ internal/db/file.go
@@ -3,5 +3,7 @@ package db
 type File struct {
 	Model
 
-	Name string `json:"name"`
+	Name   string `json:"name" gorm:"name,unique"`
+	User   User   `json:"user" gorm:"user"`
+	UserID uint   `json:"userId" gorm:"user_id"`
 }
