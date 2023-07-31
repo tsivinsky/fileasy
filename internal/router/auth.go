570a673759dabdc93bca12e6e9f486c326e675dd
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-07-31 13:48:59 +0300

    Move github-related auth handlers to separate file

--- internal/router/auth.go
+++ internal/router/auth.go
@@ -2,57 +2,14 @@ package router
 
 import (
 	"errors"
-	"fmt"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/tsivinsky/fileasy/internal/db"
-	"github.com/tsivinsky/fileasy/internal/github"
 	"github.com/tsivinsky/fileasy/internal/jwt"
 )
 
 type AuthResponse struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
-}
-
-func HandleGitHubLogin(c *fiber.Ctx) error {
-	clientId := os.Getenv("GITHUB_CLIENT_ID")
-
-	uri := fmt.Sprintf("https://github.com/login/oauth/authorize?scope=read:user&client_id=%s", clientId)
-
-	return c.Redirect(uri)
-}
-
-func HandleGitHubCallback(c *fiber.Ctx) error {
-	code := c.Query("code")
-
-	clientId := os.Getenv("GITHUB_CLIENT_ID")
-	clientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
-
-	accessToken, err := github.GetAccessToken(code, clientId, clientSecret)
-	if err != nil {
-		return err
-	}
-
-	ghUser, err := github.GetUserData(accessToken)
-	if err != nil {
-		return err
-	}
-
-	var user *db.User
-	if tx := db.Db.Where("username = ?", ghUser.Login).First(&user); tx.Error != nil {
-		user.Username = ghUser.Login
-		user.Email = ghUser.Email
-		db.Db.Create(&user)
-	}
-
-	accessToken, refreshToken, err := jwt.GenerateBothTokens(user.ID)
-	if err != nil {
-		return err
-	}
-
-	return RedirectWithTokens(c, accessToken, refreshToken)
 }
 
 type GetNewAccessTokenBody struct {
